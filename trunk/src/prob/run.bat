f127fa58810008b13291472dab07f234e3bbe7f6
ssanner <ssanner@3d5ee9d7-2a14-de47-fc28-78844542db21>
2010-12-20 09:43:21 +0000

    Updated scripts

--- trunk/src/prob/run.bat
+++ trunk/src/prob/run.bat
@@ -1,17 +1,8 @@
-@echo off
-set HOME=..
-set LIB_HOME=%HOME%\lib
-set LIBJARS=..
-for %%i in (%LIB_HOME%\*.jar) do call %HOME%\bin\cpappend.bat %%i
-if "%LIBJARS%" == "" goto staticClassPath
-goto gotLibJars
-:staticClassPath
-if exist "%LIB_HOME%\jtp.jar" set LIBJARS=%LIB_HOME%\jtp.jar
-if exist "%LIB_HOME%\antlr.jar" set LIBJARS=%LIBJARS%;%LIB_HOME%\antlr.jar
-if exist "%LIB_HOME%\xerces.jar" set LIBJARS=%LIBJARS%;%LIB_HOME%\xerces.jar
-if exist "%LIB_HOME%\jena.jar" set LIBJARS=%LIBJARS%;%LIB_HOME%\jena.jar
-if exist "%LIB_HOME%\jdom.jar" set LIBJARS=%LIBJARS%;%LIB_HOME%\jdom.jar
-if exist "%LIB_HOME%\jakarta-oro-2.0.5.jar" set LIBJARS=%LIBJARS%;%LIB_HOME%\jakarta-oro-2.0.5.jar
-:gotLibJars
-echo %LIBJARS%
-java -classpath %LIBJARS% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@@echo off
+rem Note: Need to run from JavaRelated directory.
+set LOCAL_HOME=..\..
+set LIB_HOME=%LOCAL_HOME%\lib
+set LIBJARS=%LOCAL_HOME%\bin
+for %%i in (%LIB_HOME%\*.jar) do call %LOCAL_HOME%\cpappend.bat %%i
+java -Xms200M -Xmx1024M -classpath %LIBJARS% %1 %2 %3 %4 %5 %6 %7 %8 %9
+
